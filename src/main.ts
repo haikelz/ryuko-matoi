ea30bfc3268b1aee3d85b163e0a7b09e4dbcdd12
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-02-06 15:22:34 +0700

    chore: add more args

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,27 @@ import {
 async function main() {
   const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"] },
+    puppeteer: {
+      headless: true,
+      args: [
+        "--log-level=3",
+        "--start-maximized",
+        "--no-default-browser-check",
+        "--disable-infobars",
+        "--disable-web-security",
+        "--disable-site-isolation-trials",
+        "--no-experiments",
+        "--ignore-gpu-blacklist",
+        "--ignore-certificate-errors",
+        "--ignore-certificate-errors-spki-list",
+        "--disable-gpu",
+        "--disable-extensions",
+        "--disable-default-apps",
+        "--enable-features=NetworkService",
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+      ],
+    },
   });
 
   client.on("qr", (qr: string) => {
